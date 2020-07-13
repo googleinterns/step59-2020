a1c96f05df43e17d983d98bf3a1fe626d58502f2
Aneesha Sampath <aneesha.sampath@outlook.com>
2020-07-13 14:40:14 +0000

    Allow user to make investment (hard-coded time series and number of shares)

---
+++ public/js/investment.js
@@ -0,0 +1,30 @@
+'use strict';
+
+class Investment {
+
+  constructor(symbol, share_price, num_shares) {
+    this.symbol = symbol;
+    this.share_price = share_price;
+    this.num_shares = num_shares;
+    this.total_purchase_price = share_price * num_shares;
+  }
+
+  to_dict() {
+    const data = {
+      "symbol": this.symbol,
+      "share_price": this.share_price,
+      "num_shares": this.num_shares,
+      "total_purchase_price": this.total_purchase_price
+    }
+
+    return data;
+  }
+
+  static from_dict(source) {
+    const symbol = source["symbol"];
+    const share_price = source["share_price"];
+    const num_shares = source["num_shares"];
+
+    return new Investment(symbol, share_price, num_shares);
+  }
+}
