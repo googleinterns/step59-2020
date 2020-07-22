ac4fe366f80621a25460ef8dbaddbea67689a4e1
Aneesha Sampath <aneesha.sampath@outlook.com>
2020-07-22 13:48:04 +0000

    Fix style

--- src/Components/firestore-access.js
+++ src/Components/firestore-access.js
@@ -16,7 +16,7 @@ export const setUpRoom =  async (db,NumOfSymbols,Rounds,userID) => {
     dates: datesD["dates"]
   });
   
-  await initializeQuiz(symbolsL,roomID,datesD["period"],datesD["dates"])
+  initializeQuiz(symbolsL,roomID,datesD["period"],datesD["dates"]);
 
   const usersRef = roomRef.collection('users');
   const userRef = usersRef.doc(userID);
@@ -28,7 +28,7 @@ export const setUpRoom =  async (db,NumOfSymbols,Rounds,userID) => {
     gains: 0,
     losses: 0,
   }
-  await userRef.set(gameInfo);
+  userRef.set(gameInfo);
   return roomID;
 }
 
@@ -50,14 +50,14 @@ export const getUserBalance = async (db, roomID, userID) => {
   return userData.money_left;
 }
 
-export const getChartUrl = async (db,roomId,symbol,endDate) =>{
-  let images = await db.collection('Rooms').doc(roomId).collection(symbol).doc('images').get();
+export const getChartUrl = async (db, roomID, symbol, endDate) => {
+  let images = await db.collection('Rooms').doc(roomID).collection(symbol).doc('images').get();
   let imagesData = images.data();
-  return imagesData["Stockpublic_image_url"][endDate]
+  return imagesData["Stockpublic_image_url"][endDate];
 }
 
-export const getTechnicalUrl = async (db, roomId, symbol, endDate) => {
-  let images = await db.collection('Rooms').doc(roomId).collection(symbol).doc('images').get();
+export const getTechnicalUrl = async (db, roomID, symbol, endDate) => {
+  let images = await db.collection('Rooms').doc(roomID).collection(symbol).doc('images').get();
   let imagesData = images.data();
   return imagesData["Stockpublic_image_url"][endDate]
 }
@@ -70,11 +70,13 @@ function randomDate(start, end) {
 // Minimum Period is 1Month
 export const initDates = async (db, symbols, Rounds) => {
   let Stocks= await db.collection("Ticker-Info").doc("Stock").collection("Stocks")
-    .where("Symbol","in",symbols).get()
+    .where("Symbol","in",symbols).get();
+
   let IPOyearMax = 0;
   let today = new Date();
   let year = today.getFullYear();
-  Stocks.forEach(function(Stock){
+
+  Stocks.forEach(function(Stock) {
       if (IPOyearMax < Stock.data().IPOyear){
         IPOyearMax = Stock.data().IPOyear;
       }
@@ -85,97 +87,102 @@ export const initDates = async (db, symbols, Rounds) => {
   let yearDiff = year - (IPOyearMax+1);
   let maximum_period = Math.floor(((yearDiff * 12)  - min_window_size) / Rounds);
   let random_period =  Math.floor((Math.random()  * (maximum_period - min_window_size))+min_window_size);
-  let startDate = new Date(IPOyearMax+1,1,1);
-  let endDate =  new Date(IPOyearMax+1,1+random_period,1);
+  let startDate = new Date(IPOyearMax + 1, 1, 1);
+  let endDate =  new Date(IPOyearMax + 1, 1 + random_period, 1);
   let rand_startDate =  randomDate(startDate,endDate);
   let dates = [];
   let curr_date = rand_startDate;
+
   for(var i = 0; i < Rounds; i++) {
     dates.push(curr_date.toISOString().substring(0, 10));
     curr_date = new Date(curr_date.setMonth(curr_date.getMonth()+random_period));
   }
+
   const datesD ={
     "dates" : dates,
     "period": random_period
   }
-  return datesD
+  return datesD;
 }
 
-export const initSymbols = async(db,Industry,Sector,NumOfSymbols) =>{
-  let symbols = []
-  if(Sector !== null && Industry !== null){
+export const initSymbols = async(db,Industry,Sector,NumOfSymbols) => {
 
+  let symbols = [];
+
+  if(Sector !== null && Industry !== null) {
     let formData = new FormData();
-    formData.append('Industry',Industry);
-    formData.append('Sector',Sector)
-    formData.append('NumOfSymbols',NumOfSymbols)
-    try{
+    formData.append('Industry', Industry);
+    formData.append('Sector', Sector)
+    formData.append('NumOfSymbols', NumOfSymbols);
+
+    try {
       let response = await fetch('http://localhost:8080/get-symbols', {
         method: 'POST',
         mode: 'cors',
         body: formData
-      })
-      let symbolJson = await response.json()
-      if (symbolJson.hasOwnProperty("Error")){
-        console.log("No Symbols for your query")
-        return symbols
+      });
+      let symbolJson = await response.json();
+      if (symbolJson.hasOwnProperty("Error")) {
+        console.log("No Symbols for your query");
+        return symbols;
       }
-      symbols = symbolJson['symbols']
+      symbols = symbolJson['symbols'];
     }
-    catch(error){
-      console.log("Error with Query: " + error)
+    catch (error) {
+      console.log("Error with Query: " + error);
     }
   }
 
-  else if(Industry !== null){
+  else if(Industry !== null) {
 
     let IndustryInfo =  await db.collection("Ticker-Info").doc("Industry").get();
     let numOfIndustries= IndustryInfo.data().Industry[Industry];
     let cutoff = Math.floor((Math.random()  * (numOfIndustries - NumOfSymbols))+NumOfSymbols);
     let Industries = await db.collection("Ticker-Info").doc("Stock").collection("Stocks")
-    .where("Industry","==",Industry)
-    .where("IndustryPos","<=", cutoff)
-    .orderBy("IndustryPos").limit(NumOfSymbols).get()
-    Industries.forEach(function(doc){
-      symbols.push(doc.data().Symbol)
-    })
+      .where("Industry","==",Industry).where("IndustryPos","<=", cutoff)
+      .orderBy("IndustryPos").limit(NumOfSymbols).get();
 
+    Industries.forEach(function(doc){
+      symbols.push(doc.data().Symbol);
+    });
   }
+
   else if(Sector !== null) {
 
     let SectorInfo =  await db.collection("Ticker-Info").doc("Sector").get();
     let numOfSectors= SectorInfo.data().Sector[Sector];
     let cutoff = Math.floor((Math.random()  * (numOfSectors - NumOfSymbols))+NumOfSymbols);
     let Sectors = await db.collection("Ticker-Info").doc("Stock").collection("Stocks")
-    .where("Sector","==",Sector)
-    .where("SectorPos","<=", cutoff)
-    .orderBy("SectorPos").limit(NumOfSymbols).get()
+      .where("Sector", "==", Sector).where("SectorPos", "<=", cutoff)
+      .orderBy("SectorPos").limit(NumOfSymbols).get();
+
     Sectors.forEach(function(doc){
-      symbols.push(doc.data().Symbol)
-    })
+      symbols.push(doc.data().Symbol);
+    });
   }
 
   else {
 
     let StockInfo =  await db.collection("Ticker-Info").doc("Stock").get();
     let numOfStocks = StockInfo.data().NumOfStocks - 1;
-    let cutoff = Math.floor((Math.random()  * (numOfStocks - NumOfSymbols))+NumOfSymbols);
+    let cutoff = Math.floor((Math.random() * (numOfStocks - NumOfSymbols)) + NumOfSymbols);
     let Stocks = await db.collection("Ticker-Info").doc("Stock").collection("Stocks")
-    .where("RandomPos",">=", cutoff)
-    .orderBy("RandomPos").limit(NumOfSymbols).get()
-    Stocks.forEach(function(Stock){
-      symbols.push(Stock.data().Symbol)
+      .where("RandomPos",">=", cutoff)
+      .orderBy("RandomPos").limit(NumOfSymbols).get();
+
+    Stocks.forEach(function(Stock) {
+      symbols.push(Stock.data().Symbol);
     })
 
   }
-  return symbols
+  return symbols;
 }
 
-export const initializeQuiz = async (symbols, roomId, periodLen, endDates) => {
+export const initializeQuiz = async (symbols, roomID, periodLen, endDates) => {
   var formData = new FormData();
-  formData.append('symbol',JSON.stringify(symbols));
-  formData.append('RoomId',roomId);
-  formData.append('end-date',JSON.stringify(endDates));
+  formData.append('symbol', JSON.stringify(symbols));
+  formData.append('RoomId', roomID);
+  formData.append('end-date', JSON.stringify(endDates));
   try {
     await fetch('http://localhost:8080/get-prices', {
         method: 'POST',
@@ -201,7 +208,7 @@ export const initializeQuiz = async (symbols, roomId, periodLen, endDates) => {
   }
 }
 
-export const getSymbols = async (db,roomID) =>{
+export const getSymbols = async (db, roomID) => {
   const symbol = await db.collection('Rooms').doc(roomID).get();
   const symbolData = await symbol.data();
   return symbolData.symbols;
@@ -236,15 +243,15 @@ export const getCurrentPrice = async (db, roomID) => {
 }
 
 export const advanceDay = async (db, roomID) => {
-  const roomRef = await db.collection('Rooms').doc(roomID);
+  const roomRef = db.collection('Rooms').doc(roomID);
   const data = await roomRef.get();
   const roomData = data.data();
 
-  if(roomData.day_index < roomData.dates.length){
+  if(roomData.day_index < roomData.dates.length) {
     roomRef.update({
       day_index: firebase.firestore.FieldValue.increment(1)
     });
-    return 1
+    return 1;
   }
   else {
     return 0;
