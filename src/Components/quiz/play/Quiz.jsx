cdd90e4111a758d0218d906a692cb4011f569e0d
Aneesha Sampath <aneesha.sampath@outlook.com>
2020-07-29 18:03:21 +0000

    Allow for mutliple symbols

--- src/Components/quiz/play/Quiz.jsx
+++ src/Components/quiz/play/Quiz.jsx
@@ -1,19 +1,9 @@
 import React, { Component } from 'react';
+import Select from 'react-select'
 
-import {firestore} from '../../../firebase';
-import {getUserID} from '../../../firebase';
 import {makeInvestment} from '../../firebase-access';
 
-
 /*
-inherited from Play:
-  - chart URLs
-  - roomID
-  - symbols array
-  - prices array
-  - dayIndex
-
-what Quiz does:
   - retrieves and displays user information
   - gets graph URLs from parent and displays
   - HTML form to gather all input data
@@ -22,16 +12,6 @@ what Quiz does:
     - sell (+ # of shares)
   - create changeArray to record investment in firestore
 */
-
-const NUM_SYMBOLS = 2;
-const DAY_INDEX = 0;
-const ROOMID = 'f82Cnzhyhs54aRWKKVaA';
-const USERID = '1K8yFtgBkrFr8FMd05YT';
-const CHARTURL = 'something';
-
-const BUY = "BUY";
-const SELL = "SELL";
-const HOLD = "HOLD";
 
 class Quiz extends Component {
 
@@ -42,24 +22,25 @@ class Quiz extends Component {
             password: '',
             userId: '',
             nickname: '',
-            numSymbols: 0,
-
-            //frequently updated properties
-            questionNum: 0,
+            numSymbols: null,
+            symbolSelect: [],
             chartUrls: null,
+
+            // frequently updated properties
+            questionNum: 0,
+            curChartURL: null,
             net_worth: 0,
             money_left: 0,
             curShares: [],
             prices: [],
             curBuyArray: null,
+            curSymbol: null  // symbol that's info is displayed (selected symbol)
         };
     }
 
     componentDidMount () {
-        const numSymbols = this.props.numSymbols;
-        var empArray = new Array(this.props.numSymbols).fill(0);
+        var empArray = new Array(this.state.numSymbols).fill(0);
         this.setState({
-            numSymbols: this.props.numSymbols,
             roomId: this.props.roomId,
             userId: this.props.userId,
             nickname: this.props.nickname,
@@ -70,6 +51,7 @@ class Quiz extends Component {
             curShares: this.props.curShares,
             prices: this.props.prices,
             curBuyArray: empArray,
+            symbolSelect: this.getSymbolData()
         });
     }
 
@@ -81,141 +63,100 @@ class Quiz extends Component {
             money_left: props.money_left,
             curShares: props.curShares,
             prices: props.prices,
+            numSymbols: props.numSymbols
         }
     }
 
     getSymbolData = () => {
-        let rows = [];
-
-        // get current price and user shares held for each of the symbols
-        // also present the BUY/HOLD/SELL options
-        for (let i = 0; i < NUM_SYMBOLS; i++) {
-            const currentPrice = this.getPrice(i);
-            const numShares = this.state.userShares[i];
-
-            const toRender = (
-                <div key={i}>
-                    <p>Symbol{i}:</p>
-                    <p>Current price is: {currentPrice}.</p>
-                    <p>You currently hold {numShares} share(s).</p>
-
-                    <button type="button">BUY</button>
-                    <input type="number" placeholder="10" onChange={(event) => this.updateFormState(event, i, BUY)}/>
-
-                    <button type="button" onClick={(event) => this.updateFormState(event, i, HOLD)}>HOLD</button>
-
-                    <button type="button">SELL</button>
-                    <input type="number" placeholder="10" onChange={(event) => this.updateFormState(event, i, SELL)}/>
-                    <br/>
-                </div>
-            );
-            rows.push(toRender);
-            this.addRowToFormData(i);
-        }
-
-        return rows;
+        const newSymbolSelect = new Array(this.state.numSymbols).fill(0);
+        for (let i = 0; i < newSymbolSelect.length; i++) {
+            const selectLabel = 'Symbol ' + i;
+            newSymbolSelect[i] = {label: selectLabel, value: i};
+        } 
+        
+        return newSymbolSelect;
     }
 
-    // update state variable to reflect number of shares
-    updateFormState = (event, index, state) => {
-        var newState = {
-            BUY: null,
-            HOLD: null,
-            SELL: null
-        };
-
-        // handle different states
-        if (state === BUY) {
-            var numShares = event.target.value;
-
-            // value is deleted
-            if (!numShares) {
-                this.state.formInput[index][BUY] = null;
-                return;
-            }
-
-            newState[BUY] = Number(numShares);
-        }
-        else if (state === HOLD) {
-            newState[HOLD] = 1;  // indicate that user wants to Hold this investment with boolean flag
-        }
-        else if (state === SELL) {
-            var numShares = event.target.value;
-
-            // deletion
-            if (!numShares) {
-                this.state.formInput[index][SELL] = null;
-                return;
-            }
-
-            newState[SELL] = Number(numShares);
-        }
-        else {
-            throw "An unexpected error occurred when trying to process the state";
-        }
-
-        this.state.formInput[index] = newState;
+    // update the symbol being traded
+    symbolSelectHandler = (selectedSymbol) => {
+        this.setState({
+            curSymbol: selectedSymbol.value,
+        });
     }
 
-    //parse form submission results and convert into a changeArray (for firestore)
-    parseResults = () => {
-        var changeArray = [];
-        for (let i = 0; i < this.state.formInput.length; i++) {
-            const currentRow = this.state.formInput[i];
-
-            if (currentRow[BUY]) {
-                changeArray.push(currentRow[BUY]);
-            }
-            else if (currentRow[HOLD]) {
-                changeArray.push(0);  // invest 0 in symbol
-            }
-            else if (currentRow[SELL]) {
-                changeArray.push((currentRow[SELL] * -1));
-            }
-            else {
-                alert("Please enter an action for each symbol");
-                break;
-            }
-        }
-
-        this.validate(changeArray);
-        makeInvestment(ROOMID, this.state.userID, DAY_INDEX, changeArray);
-        alert('investment recorded');
+    submitHandler = (event) => {
+        event.preventDefault();
     }
 
-    // TODO make this implement buyArray instead of just buying one share of the first symbol
-    buy() {
+    buy = (event) => {
         const {roomId, userId, questionNum} = this.state;
-        var arr = new Array(this.props.numSymbols).fill(0);
-        arr[0] = 1;
-        makeInvestment(roomId,userId,questionNum,arr);
+        var changeArray = new Array(this.state.numSymbols).fill(0);
+
+        var numShares = null;
+
+        // reset to 0 if user deletes the value
+        !event.target.value ? numShares = 0 : numShares = event.target.value;
+        changeArray[this.state.curSymbol] = numShares;
+        
+        if(!makeInvestment(roomId,userId,questionNum,changeArray)) {
+            alert('Not a valid transaction - please try again.');
+        }
     }
 
-    sell() {
+    sell = (event) => {
         const {roomId, userId, questionNum} = this.state;
-        var arr = new Array(this.props.numSymbols).fill(0);
-        arr[0] = -1;
-        makeInvestment(roomId,userId,questionNum,arr);
+        var changeArray = new Array(this.state.numSymbols).fill(0);
+        changeArray[this.state.curSymbol] = (-1 * event.target.value);
+
+        var numShares = null;
+
+        !event.target.value ? numShares = 0 : numShares = (-1 * event.target.value);
+        changeArray[this.state.curSymbol] = numShares;
+
+        if(!makeInvestment(roomId,userId,questionNum,changeArray)) {
+            alert('Not a valid transaction - please try again.');
+        } 
     }
 
     render() {
-        const {questionNum,chartUrls,net_worth,money_left,curShares,prices,curBuyArray,nickname,roomId} = this.state;
-        var imgUrls = chartUrls[0];
+        const {questionNum,chartUrls,net_worth,money_left,curShares,
+          prices,curBuyArray,nickname,roomId,symbolSelect, curSymbol} = this.state;
+
+        // conditionally render the symbol info - only render the symbol's
+        // info that is selected from the dropdown
         return (
             <div>
-                <p>room id: {roomId}</p>
-                <p>nickname: {nickname}</p>
-                <p>question number: {questionNum}</p>
-                <p>chart:</p>
-                <img src={imgUrls[0]}/>
-                <p>net worth: {net_worth}</p>
-                <p>cash: {money_left}</p>
-                <p>current shares of symbol: {curShares[0]}</p>
-                <p>price of symbol: {prices[0]}</p>
-                <button onClick={() => this.buy()}>buy</button>
-                <button onClick={() => this.sell()}>sell</button>
+                <p>Hi there, {nickname}!</p>
+                <p>You are in Room {roomId}.</p>
+
+                <p>Your net worth is ${net_worth}.</p>
+                <p>Your account balance is currently ${money_left}.</p>
+
+                <form onSubmit={this.submitHandler}>
+                    <Select onChange={this.symbolSelectHandler} options={symbolSelect} />
+                    <br/><br/>
+                    
+                    {curSymbol != null && 
+                        <div>
+                            <p>Selected symbol: Symbol {curSymbol}</p>
+                            <p>You hold {curShares[curSymbol]} shares.</p>
+                            <p>The price is currently ${prices[curSymbol]}.</p>
+                            <img src={chartUrls[curSymbol]} />
+
+                            <label>Buy:</label>
+                            <input type='number' onChange={this.buy} />
+
+                            <button type='button'>HOLD</button>
+
+                            <label>Sell:</label>
+                            <input type='number' onChange={this.sell} />
+                        </div>
+                    }
+                </form>
+
+
             </div>
-        );
+        )
     }
 };
 
